851565b33caf0af21b804e1b721c0ff2129a7d60
jvg21 <75985123+jvg21@users.noreply.github.com>
2022-06-22 14:32:37 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+document.getElementById("btn_menu").onclick = function menu(){
+    document.location.reload(true)
+}
+document.getElementById("btn_home").onclick = function Shome(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+document.getElementById("btn_sobre").onclick = function Shome(){
+    document.body.scrollTop = 445;
+    document.documentElement.scrollTop = 445;
+}
+document.getElementById("btn_Projeto").onclick = function Shome(){
+    document.body.scrollTop = 1000;
+    document.documentElement.scrollTop = 1000;
+}
+document.getElementById("btn_contato").onclick = function Shome(){
+    document.body.scrollTop = 3000;
+    document.documentElement.scrollTop = 3000;
+}
+document.getElementById("Linkedin").onclick = function Linkedin(){
+    window.open("https://www.linkedin.com/in/joão-vitor-gregorini-72852717b/")
+}
+document.getElementById("Instagram").onclick = function Instagram(){
+    window.open("https://www.instagram.com/jvgregorini/")
+}
